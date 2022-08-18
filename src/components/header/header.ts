3772d0eb486614b5e15bb0bcfb8984891f957d75
alisafed <alisa.fed.91@gmail.com>
2022-08-19 00:38:37 +0300

    refactor: rename classes and styles

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,4 +1,4 @@
-export const createFooter = () => {
+export const createHeader = () => {
     const body = document.querySelector('body') as HTMLBodyElement;
     const header = document.createElement('header')
     header.classList.add('header')
@@ -12,11 +12,11 @@ export const createFooter = () => {
     const button_burger = document.createElement('button')
     button_burger.classList.add('button_burger')
     header_components.appendChild(button_burger)
-
+    
     const button_burger_box = document.createElement('span')
     button_burger_box.classList.add('button_burger_box')
     button_burger.appendChild(button_burger_box)
-
+    
     const button_burger_box_img = document.createElement('img')
     button_burger_box_img.classList.add('button_burger_box_img')
     button_burger_box.appendChild(button_burger_box_img)
@@ -31,7 +31,7 @@ export const createFooter = () => {
     const header_content_page = document.createElement('div')
     header_content_page.classList.add('header_content_page')
     header_content.appendChild(header_content_page)
-
+    
     const header_content_page_name = document.createElement('h1')
     header_content_page_name.classList.add('header_content_page_name')
     header_content_page.appendChild(header_content_page_name)
@@ -41,21 +41,21 @@ export const createFooter = () => {
     const header_content_user_box = document.createElement('div')
     header_content_user_box.classList.add('header_content_user_box')
     header_content.appendChild(header_content_user_box)
-
+    
     const header_content_user = document.createElement('div')
     header_content_user.classList.add('header_content_user')
     header_content_user_box.appendChild(header_content_user)
-
+    
     const header_content_user_name = document.createElement('p')
     header_content_user_name.classList.add('header_content_user_name')
     header_content_user.appendChild(header_content_user_name)
-    header_content_user_name.textContent = 'Jone Doe' //`${user_name}`
-
+    header_content_user_name.textContent = 'Jone Doe' //`${login_name}`
+    
     const header_content_user_email = document.createElement('p')
     header_content_user_email.classList.add('header_content_user_email')
     header_content_user.appendChild(header_content_user_email)
-    header_content_user_email.textContent = 'jondoe@gmail.com' //`${user_email}`
-
+    header_content_user_email.textContent = 'jondoe@gmail.com' //`${login_email}`
+    
     const header_content_user_img_box = document.createElement('div')
     header_content_user_img_box.classList.add('header_content_user_img_box')
     header_content_user_box.appendChild(header_content_user_img_box)
@@ -71,7 +71,7 @@ export const createFooter = () => {
     header_content.appendChild(header_content_login)
 
     const header_content_login_button = document.createElement('button')
-    header_content_login_button.classList.add('main_page_button', 'custom-btn')
+    header_content_login_button.classList.add('main_page_button', 'custom-btn', 'open_pop_login')
     header_content_login.appendChild(header_content_login_button)
     header_content_login_button.textContent = 'LOGIN'
 }

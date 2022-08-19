e4f7cba83c65e457a3b039a3e15b098bd15d0b74
alisafed <alisa.fed.91@gmail.com>
2022-08-19 19:29:19 +0300

    feat: add index file with function

---
+++ src/components/index/index.ts
@@ -0,0 +1,11 @@
+import { createFifthSection } from "./fifth-section";
+import { createFirstSection } from "./first-section";
+import { createFourthSection } from "./fourth-section";
+import { createSecondSection } from "./second-section";
+import { createThirdSection } from "./third-section";
+
+createFirstSection();
+createSecondSection();
+createThirdSection();
+createFourthSection();
+createFifthSection();

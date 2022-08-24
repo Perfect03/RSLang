cb59c46544edc7e43d0d180a5a35949a3ffb073a
alisafed <alisa.fed.91@gmail.com>
2022-08-24 23:40:58 +0300

    feat: add statistics page

--- src/components/statistics/index.ts
+++ src/components/statistics/index.ts
@@ -1,6 +1,3 @@
-import { createStatistic } from './stat';
 import './stat.css';
 import '../components/statistics/assets/sprint.png';
 import '../components/statistics/assets/audio.png';
-
-createStatistic();

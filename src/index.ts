5dbbf19ff7831134b4aef93dc9e12703688a4614
Falderian <sneograt@gmail.com>
2022-09-01 16:39:34 +0300

    fix: fix conflict

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ import './components/header/index';
 import './components/index/index';
 import './components/footer/index';
 import './components/popup/index';
-import './components/minigames/audiochallenge/index';

bc08d69f5d8cbde2543c54a51f66fb5c6748d3d7
alisafed <alisa.fed.91@gmail.com>
2022-09-01 21:53:24 +0300

    refactor: delete one div in mainpage

--- src/api/authorization/signin.ts
+++ src/api/authorization/signin.ts
@@ -1,9 +1,9 @@
-import { signFunction } from '../../components/popup/signup';
+//import { signFunction } from '../../components/popup/signup';
 import { UserInfo } from '../../interfaces & types/authorization';
 import { baseUrl } from '../api';
 
 export const createUser = async (user: UserInfo) => {
-    const rawResponse = await fetch(`${baseUrl}/users`, {
+    const rawResponse = await fetch(`${baseUrl}users`, {
         method: 'POST',
         headers: {
             Accept: 'application/json',

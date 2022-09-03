2219bc3db0bd20ed372290161e04556b9b88fa07
Falderian <sneograt@gmail.com>
2022-09-03 12:23:49 +0300

    feat: change ui for auth user

--- src/components/popup/login-authorization.ts
+++ src/components/popup/login-authorization.ts
@@ -6,15 +6,22 @@ export const addLoginRequest = () => {
     const loginSave = document.querySelector('.login-save') as HTMLButtonElement;
     loginSave.addEventListener('click', async function () {
         await loginUser(loginFunction());
-        getUserInfo(storageUserAccInfo.userId);
+        await getUserInfo(storageUserAccInfo.userId);
         console.log(storageUserAccInfo);
-
+        changeUiForAuthOr();
     });
 };
 
-const changeUiForAuthOr = (condition: boolean) => {
-    const open_pop_login = document.querySelector('.open_pop_login') as HTMLButtonElement;
+const changeUiForAuthOr = () => {
+    const header_content_login = document.querySelector('.header_content_login') as HTMLButtonElement;
+    const header_content_user_box = document.querySelector('.header_content_user_box') as HTMLButtonElement;
+    const header_content_user_name = document.querySelector('.header_content_user_name') as HTMLDivElement;
+    const header_content_user_email = document.querySelector('.header_content_user_email') as HTMLDivElement;
+
     if (storageUserAccInfo.message === 'Authenticated') {
-        open_pop_login.style.display = 'none';
-    }
-}
+        header_content_login.style.display = 'none';
+        header_content_user_box.style.display = 'flex';
+        header_content_user_name.textContent = storageUserAccInfo.name;
+        header_content_user_email.textContent = storageUserAccInfo.email;
+    } 
+};

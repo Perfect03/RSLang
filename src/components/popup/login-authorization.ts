093f5425841170cf9e5517f08e9a6886523cdda1
Falderian <sneograt@gmail.com>
2022-09-03 12:02:24 +0300

    feat: add ui func change

--- src/components/popup/login-authorization.ts
+++ src/components/popup/login-authorization.ts
@@ -3,14 +3,18 @@ import { storageUserAccInfo } from '../utils/storage';
 import { loginFunction } from './login';
 
 export const addLoginRequest = () => {
-    const open_pop_login = document.querySelector('.open_pop_login') as HTMLButtonElement;
     const loginSave = document.querySelector('.login-save') as HTMLButtonElement;
     loginSave.addEventListener('click', async function () {
         await loginUser(loginFunction());
         getUserInfo(storageUserAccInfo.userId);
         console.log(storageUserAccInfo);
-        if (storageUserAccInfo.message === 'Authenticated') {
-            open_pop_login.style.display = 'none';
-        }
+
     });
 };
+
+const changeUiForAuthOr = (condition: boolean) => {
+    const open_pop_login = document.querySelector('.open_pop_login') as HTMLButtonElement;
+    if (storageUserAccInfo.message === 'Authenticated') {
+        open_pop_login.style.display = 'none';
+    }
+}
